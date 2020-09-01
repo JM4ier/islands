3eeb5d1eb2c9a9b07f79173304281fb9ccbb5519
Jonas Maier <1.jmaier.3@gmail.com>
2020-09-01 21:25:54 +0200

    src/river.rs: add comments, remove unnecessary test

--- src/river.rs
+++ src/river.rs
@@ -1,15 +1,24 @@
 use crate::{map::*, flow::*};
 
+/// Calculates the flow map of the terrain given in `map`.
+///
+/// It does so by distributing water on each cell of the map and drawing
+/// all of the waters path onto the flow_map, which it returns.
+///
 pub fn create_flow_map(map: &Map, range: usize) -> Map {
     let width = map.width();
     let height = map.height();
     let mut flow_map = Map::new(width, height);
-
     let circle = circle(range);
 
-    let mut targets = vec![vec![0isize; height]; width];
+    assert!(range < (1 << 13));
+
+    // how many other cells 'target' this cell
+    let mut targets = vec![vec![0i32; height]; width];
+    // how much volume flows through each cell
     let mut volume = vec![vec![1.0f32; height]; width];
 
+    // finding how many times each cell is targeted
     for x in range..(width-range) {
         for y in range..(height-range) {
             let (nx, ny) = next_target(map, x, y, &circle);
@@ -21,25 +30,25 @@ pub fn create_flow_map(map: &Map, range: usize) -> Map {
 
     for x in range..(width-range) {
         for y in range..(height-range) {
-
-            let (mut x, mut y) = (x, y);
             if targets[x][y] == 0 {
+                let (mut x, mut y) = (x, y);
                 loop {
+                    // prevent processing this cell twice
                     targets[x][y] = -1;
+
                     if x < range || x >= width-range || y < range || y >= height-range {
                         break;
                     }
 
                     let (nx, ny) = next_target(map, x, y, &circle);
-                    if (nx, ny) == (x, y) {
-                        break;
-                    }
 
                     draw_line(&mut flow_map, x as _, y as _, nx as _, ny as _, &|h| h + volume[x][y]);
                     volume[nx][ny] += volume[x][y];
                     targets[nx][ny] -= 1;
 
                     if targets[nx][ny] == 0 {
+                        // if all the cells which target (nx, ny) have been processed, (nx, ny) can
+                        // now be processed
                         x = nx;
                         y = ny;
                     } else {
@@ -51,12 +60,5 @@ pub fn create_flow_map(map: &Map, range: usize) -> Map {
     }
 
     flow_map
-}
-
-#[test]
-fn points_are_correctly_sorted() {
-    let mut points = vec![Point{ x: 0, y: 0, z: 1.0 }, Point{ x: 4, y: 6, z: 7.5 }];
-    points.sort();
-    assert!(points[0].z > points[1].z);
 }
 
